710d4aa6bee337081e8d896e997dd48f42231bf2
marrero171 <50031007+marrero171@users.noreply.github.com>
2026-05-06 23:22:47 -0400

    Updated GEQO

--- examples/3d/entities/test_player.gd
+++ examples/3d/entities/test_player.gd
@@ -14,11 +14,11 @@ func _physics_process(delta: float) -> void:
 		return
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
