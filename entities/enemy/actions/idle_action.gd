6504fac84f2f0e44c31780acbfff98cfd06a953b
marrero171 <50031007+marrero171@users.noreply.github.com>
2026-05-15 11:50:33 -0400

    Added spotted_player condition

--- entities/enemy/actions/idle_action.gd
+++ entities/enemy/actions/idle_action.gd
@@ -3,4 +3,5 @@ extends ActionLeaf
 
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	actor.velocity = Vector3.ZERO
+	print_debug("Idled")
 	return SUCCESS
---
+++ entities/enemy/conditions/spotted_player_condition.gd
@@ -0,0 +1,27 @@
+@tool
+extends ConditionLeaf
+
+@export_flags_3d_physics var collision_mask: int
+
+var raycast: RayCast3D
+var player: Player
+
+func before_run(actor: Node, blackboard: Blackboard) -> void:
+	if not raycast:
+		raycast = RayCast3D.new()
+		actor.add_child(raycast)
+		raycast.collision_mask = collision_mask
+	else:
+		raycast.enabled = true
+	if not player:
+		player = get_tree().get_first_node_in_group("player")
+
+func tick(actor: Node, blackboard: Blackboard) -> int:
+	raycast.target_position = raycast.to_local(player.global_position)
+	raycast.force_raycast_update()
+	if raycast.get_collider() == player:
+		return SUCCESS
+	return FAILURE
+
+func after_run(actor: Node, blackboard: Blackboard) -> void:
+	raycast.enabled = false
