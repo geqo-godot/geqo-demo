a14c70fc3f4c4de0531b27ce27c2817984926128
marrero171 <50031007+marrero171@users.noreply.github.com>
2026-05-16 14:27:28 -0400

    Strafing Guy AI WIP

--- entities/enemy/actions/approach_target_action.gd
+++ entities/enemy/actions/approach_target_action.gd
@@ -2,21 +2,37 @@
 extends ActionLeaf
 ## Get closer to a target until min range is reached. Needs a target to work.
 
+enum TargetType { POSITION, NODE }
+
 ## How close the enemy is to target to succeed
 @export var min_range: float = 3
 @export var blackboard_key: String = "query_result"
+@export var target_type: TargetType
 
 var current_target: Vector3
-var final_target: Vector3
+var final_target
 
 func before_run(actor: Node, blackboard: Blackboard) -> void:
 	final_target = blackboard.get_value(blackboard_key)
+	match target_type:
+		TargetType.NODE:
+			final_target = final_target.global_position
+	
 	actor.nav_agent.target_position = final_target
 
 func tick(actor: Node, blackboard: Blackboard) -> int:
+	var target_pos: Vector3
+	match target_type:
+		TargetType.POSITION:
+			target_pos = final_target
+		TargetType.NODE:
+			target_type = final_target.global_position
+	
+	if target_type == TargetType.NODE:
+		actor.nav_agent.target_position = final_target.global_position
 	current_target = actor.nav_agent.get_next_path_position()
 	actor.move_to_target(current_target)
 	
-	if actor.global_position.distance_to(final_target) < min_range:
+	if actor.global_position.distance_to(target_pos) < min_range:
 		return SUCCESS
 	return RUNNING
